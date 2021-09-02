2df85c6f8b6ecddabc1061015ad4222f6baca1cd
Ayush Dubey <ayush.dubey.beehyv@alation.com>
2021-09-02 08:10:28 +0530

    Create file canSum.py

---
+++ DynamicProgramming/canSum.py
@@ -0,0 +1,29 @@
+def canSum(array, target):
+    if target == 0:
+        return False
+    memo = canSumHelper(array, target, {}, 0)
+    print(memo)
+    return memo.get(target)
+
+
+def canSumHelper(array, target, memo, elem):
+    if target in memo or target < 0:
+        return memo
+    if target == 0:
+        memo[elem] = True
+        return memo
+    for number in array:
+        newTarget = target - number
+        memo = canSumHelper(array, newTarget, memo, number)
+        if newTarget in memo and memo[newTarget]:
+            memo[target] = True
+            return memo
+    if target not in memo:
+        memo[target] = False
+    return memo
+
+
+if __name__ == '__main__':
+    array = [14, 7]
+    target = 300
+    print(canSum(array, target))

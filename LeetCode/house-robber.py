57ae56e7df13b285b4efa9837807e031fc1afd28
ayush286 <89552326+ayush286@users.noreply.github.com>
2021-09-09 21:33:15 +0530

    Create house-robber.py

---
+++ LeetCode/house-robber.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        chosen = None
+        notChosen = None
+        for loot in nums:
+            if chosen is None:
+                chosen = loot
+                notChosen = 0
+                continue
+            newChosen = notChosen + loot
+            newNotChosen = max(chosen, notChosen)
+            chosen = newChosen
+            notChosen = newNotChosen
+        return max(chosen, notChosen)
+        
